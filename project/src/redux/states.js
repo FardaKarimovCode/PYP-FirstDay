6cf562b29693bc302154111857c6b8dab6144afe
Karimov-Farda <83571454+Karimov-Farda@users.noreply.github.com>
2021-05-26 18:37:03 +0400

    All assets uploaded

---
+++ project/src/redux/states.js
@@ -0,0 +1,18 @@
+export let items = [
+    {
+        id : 1,
+        name : "First"
+    },
+    {
+        id : 2,
+        name : "Second"
+    },
+    {
+        id : 3,
+        name : "Third"
+    },
+    {
+        id : 4,
+        name : "Fourth"
+    },
+]
